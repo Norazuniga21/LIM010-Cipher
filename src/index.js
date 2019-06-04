eecee8f34ebfe5aa25d9289c3db4d45c85b6e233
Norazuniga21 <norazugu2106@gmail.com>
2019-06-04 13:05:17 -0500

    Subiendo proyecto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-/* Acá va tu código */
+
+const ingreso = document.getElementById("ingreso");
+ingreso.addEventListener('click', () => {
+    const contrasena = document.getElementById("contrasena_lab").value;
+    if (contrasena=="LABORATORIA") {
+    document.getElementById("pantalla1").classList.add('hide');
+    document.getElementById("pantalla2").classList.remove('hide');
+      } else {
+        document.getElementById("error").innerHTML="Contraseña incorrecta, intente de nuevo";
+        
+      }
+        
+});         
