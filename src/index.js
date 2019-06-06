7eecc475684d64cb325a2bedbe4f35a07f477469
Norazuniga21 <norazugu2106@gmail.com>
2019-06-06 13:00:23 -0500

    Cambio en css

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
-(function(){
-  let password = 0;
-  function validar(){
+let password = 0;
+ const ingreso = document.getElementById("ingreso");
+ ingreso.addEventListener("click",()=>{
+   if(password<=3){
     const contrasena = document.getElementById("contrasena_lab").value;
     if (contrasena == "LABORATORIA") {
       document.getElementById("pantalla1").classList.add('hide');
       document.getElementById("pantalla2").classList.remove('hide');
     }
-    else {
+    else{
       document.getElementById("error").innerHTML="Contraseña incorrecta, intente de nuevo";
       password=password + 1;
-      if (password==3){
+      if(password>2){
         document.getElementById("error").innerHTML="Numero de intentos excedidos, regrese en 6 horas";
         document.getElementById("contrasena_lab").disabled = true;
         document.getElementById("ingreso").disabled = true;
-      }
-    }  
-  };
-const ingreso = document.getElementById("ingreso");
-ingreso.addEventListener("click",validar);
-}());
-
-
-
-
-
+      }}}}
+ );
+     
 
 
