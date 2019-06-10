8738b3a3e4280edd1a6add5776866912da0d37db
Norazuniga21 <norazugu2106@hotmail.com>
2019-06-10 01:39:26 -0500

    subiendo cambios en cipher.js y index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,22 @@
 window.cipher = {
   encode: (offset, string) => {
-    /* Acá va tu código que cifra*/
+    let mensajeCifrado = "";
+    for (let i = 0;i = i< string.length;i++){
+      let caracter = string[i];
+      if (caracter.matchmatch(/[a-z]/i)) {
+        if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+          let textoChar = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+          mensajeCifrado += String.fromCharCode(textoChar);
+        } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+          textoChar = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
+          mensajeCifrado += String.fromCharCode(textoChar);
+        }  
+      } else {
+        mensajeCifrado += caracter;
+    }
+}
+return mensajeCifrado;    
+
   },
   decode: (offset, string) => {
     /* Acá va tu código que descifra*/
--- src/index.js
+++ src/index.js
@@ -16,6 +16,31 @@ let password = 0;
         document.getElementById("ingreso").disabled = true;
       }}}}
  );
-     
 
+/*variables generales*/
+const msj = document.getElementById("mensaje_pet");
+const offset= document.getElementById("offset_pet");
+
+const msjCodificar = document.getElementById("btnCodificar");
+btnCodificar.addEventListener("click", () => {
+  const msjCod = msj.value;
+  const off = offset.value;
+  document.getElementById("mensajeCheck").innerHTML = window.cipher.encode(msj,offset);
+});  
+
+const msjDecodificar = document.getElementById("btnDecodificar");
+btnDecodificar.addEventListener("click",()=>{
+  let msj = document.getElementById("mensaje_pet").value;
+  let offset= document.getElementById("offset_pet").value;
+  document.getElementById("mensajeCheck").innerHTML = window.cipher.decode(msj,offset);
+});  
+
+/*let borrar = document.getElementById("borrar");
+borrar.addEventListener("click",()=>{
+  let mensaje_pet = 
+  
+  for(let i = 0; i < td.length; i++){
+    borrar[i].innerHTML = ''
+  }
+});  */
 
